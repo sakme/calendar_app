af18640a8158571ef9888b5938acd48d1a3bcee2
sakme <alan@akme.us>
2021-11-28 14:07:52 -0500

    item edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,80 @@
 var today = moment().format("dddd, MMMM Do YYYY");
-var items = [
-  {
-    time = 8,
-    text = "temp"
-  },
-  {
-    time = 9,
-    text = "temp"
-  },
-  {
-    time = 10,
-    text = "temp"
-  },
-  {
-    time = 11,
-    text = "temp"
-  },
-  {
-    time = 12,
-    text = "temp"
-  },
-  {
-    time = 1,
-    text = "temp"
-  },
-  {
-    time = 2,
-    text = "temp"
-  },
-  {
-    time = 3,
-    text = "temp"
-  },
-  {
-    time = 4,
-    text = "temp"
-  },
-  {
-    time = 5,
-    text = "temp"
-  }
-];
-
 $("#currentDay").text(today);
 
+var items = [];
+
 var hour = moment().hour();
-console.log(hour);
+
+// load items
 var loadItems = function() {
   items = JSON.parse(localStorage.getItem("items"));
 
   // if nothing in localStorage, create a new object to track all task status arrays
   if (!items) {
-      items = [];
+      items = [
+        {
+          "time": 8,
+          "text": "temp"
+        },
+        {
+          "time": 9,
+          "text": "temp"
+        },
+        {
+          "time": 10,
+          "text": "temp"
+        },
+        {
+          "time": 11,
+          "text": "temp"
+        },
+        {
+          "time": 12,
+          "text": "temp"
+        },
+        {
+          "time": 13,
+          "text": "temp"
+        },
+        {
+          "time": 14,
+          "text": "temp"
+        },
+        {
+          "time": 15,
+          "text": "temp"
+        },
+        {
+          "time": 16,
+          "text": "temp"
+        },
+        {
+          "time": 17,
+          "text": "temp"
+        }];
   }
-
+  
   // loop over object properties
   for (i=0; i < items.length; i++) {
       createItem(items[i].time, items[i].text,);
     };
 };
 
-var saveItems = function() {
-    localStorage.setItem("items", JSON.stringify(items));
-};
-
+// create items
 var createItem = function(itemTime, itemText) {
   var itemDiv = $("#b" + itemTime);
 
   var itemP = $("<p>").text(itemText);
-
+  
   var evalTime = function() {
       if ((itemTime) < hour){
         $("#b" + itemTime).addClass("past");
-      } 
-      else if ((itemTime) < hour) {
-        $("#b" + itemTime).addClass("present");
-      } 
-      else {
+      }
+      else if ((itemTime) > hour) {
         $("#b" + itemTime).addClass("future");
+      }
+      else {
+        $("#b" + itemTime).addClass("present");
       }
   }
 
@@ -86,12 +83,17 @@ var createItem = function(itemTime, itemText) {
 
     $(itemDiv)
       .append(itemP);
+
+    saveItems();
 };
 
-  // select task text to edit
-$(".description").on("click", "p", function() {
-  // $(this).append("<p>");
+// save items
+var saveItems = function() {
+    localStorage.setItem("items", JSON.stringify(items));
+};
 
+// select task text to edit
+$(".description").on("click", "p", function() {
   var text = $(this)
     .text()
     .trim();
@@ -100,8 +102,6 @@ $(".description").on("click", "p", function() {
     .addClass("textarea")
     .val(text);
 
-    console.log(textInput);
-
   $(this).replaceWith(textInput);
 
   textInput.trigger("focus");
@@ -109,27 +109,26 @@ $(".description").on("click", "p", function() {
 
 // save edited task text
 $(".description").on("blur", "textarea", function() {
-  // get current value
-  var text = $(this)
-    .val()
-    .trim();
-    console.log(text);
-
   // get the parent name attribute
-  var time = $(this)
+  var timeVal = $(this)
     .closest(".description")
     .attr("name")
 
-   items.push({
-     time: time,
-     text: text
-   }) 
+  // get current value
+  var textVal = $(this)
+    .val()
+    .trim();
+
+  index = items.findIndex((obj => obj.time == timeVal));
+  
+  items[index].text = textVal;
+    
   
   saveItems();
 
-    // replace p element
-    var itemP = $("<p>")
-      .text(text);
+  // replace p element
+  var itemP = $("<p>")
+    .text(textVal);
 
   // replace textarea with p element
   $(this).replaceWith(itemP);
